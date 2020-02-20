656215df2802ea1e5defe3349d996c96d9a3599e
mathias.ressort <mathias.ressort@epitech.eu>
2020-02-20 19:45:39 +0100

    [Update] Structure

--- include/hashcode.h
+++ include/hashcode.h
@@ -1,6 +1,19 @@
 #ifndef LS_H_
 #define LS_H_
 
+struct book {
+    int id;
+    int score;
+};
+
+struct lib {
+    bool is_signed_up;
+    int nb_books;
+    int signup_duration;
+    int books_per_day;
+    struct book books[];
+};
+
 int hashcode(int argc, char **argv);
 
 #endif /* !LS_H_ */
